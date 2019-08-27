2b1ed154a83715260df3de4d4da6d9000a2b8a3d
Sajib Biswas <sajibcse28@gmail.com>
2019-08-27 22:44:04 +0600

    Updates

---
+++ precache-manifest.37c282bdb28a759efa47574563b7be8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa308f356ae00acaf070e2ffe2dda60",
+    "url": "/vimory-web/index.html"
+  },
+  {
+    "revision": "5ca10c7c7841e4323464",
+    "url": "/vimory-web/static/css/2.38a01fbb.chunk.css"
+  },
+  {
+    "revision": "9ed9241f381cc53c7851",
+    "url": "/vimory-web/static/css/main.da438c49.chunk.css"
+  },
+  {
+    "revision": "5ca10c7c7841e4323464",
+    "url": "/vimory-web/static/js/2.9676c0fa.chunk.js"
+  },
+  {
+    "revision": "9ed9241f381cc53c7851",
+    "url": "/vimory-web/static/js/main.b859f97e.chunk.js"
+  },
+  {
+    "revision": "727c903ffb130adc3f72",
+    "url": "/vimory-web/static/js/runtime~main.af8c2dd2.js"
+  },
+  {
+    "revision": "05556633579a10179eb2dcc042e5a970",
+    "url": "/vimory-web/static/media/effect-bg.05556633.jpg"
+  },
+  {
+    "revision": "fd3529311395a04f61aa364669c6f426",
+    "url": "/vimory-web/static/media/intro-bg.fd352931.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/vimory-web/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/vimory-web/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/vimory-web/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/vimory-web/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2c0dbc8c52cdb431ccd74b33ac29d3a2",
+    "url": "/vimory-web/static/media/slide-bg.2c0dbc8c.jpg"
+  },
+  {
+    "revision": "9057f6689db025fc42d187a6dbd96d90",
+    "url": "/vimory-web/static/media/template-bg.9057f668.jpg"
+  }
+]);
